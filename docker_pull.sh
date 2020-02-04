99e2db76e3f29b48c118bab4bfdd50d144ea2a2c
Thiago Fontes <thiago93br@gmail.com>
2020-02-04 00:37:21 -0300

    Adding script to download docker images

---
+++ docker_pull.sh
@@ -0,0 +1,5 @@
+docker pull hyperledger/fabric-peer:1.2.1
+docker pull hyperledger/fabric-ccenv:1.2.1
+docker pull hyperledger/fabric-orderer:1.2.1
+docker pull hyperledger/fabric-ca:1.2.1
+docker pull hyperledger/fabric-couchdb:0.4.10

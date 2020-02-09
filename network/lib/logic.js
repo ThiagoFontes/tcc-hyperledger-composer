25633f0d0f345af3bf73cd3d9c1dd2b20764ce5e
Thiago Fontes <thiago93br@gmail.com>
2020-02-09 00:28:30 -0300

    Updating models and defining transactions

--- network/lib/logic.js
+++ network/lib/logic.js
@@ -1,26 +1,22 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 /**
- * Write your transction processor functions here
- */
-/**
- * Track the trade of a commodity from one trader to another
- * @param {dev.thiagofontes.Trade} trade - the trade to be processed
+ * Track the device acess permission and ownership 
+ * @param {dev.thiagofontes.givePermission} tx - the device to be processed
  * @transaction
  */
-async function tradeCommodity(trade) {
-    trade.commodity.owner = trade.newOwner;
-    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
-    await assetRegistry.update(trade.commodity);
+async function givePermission(tx) {
+  tx.device.users.push(tx.addedUser);
+
+  const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
+  await assetRegistry.update(tx.device);
+}
+
+async function revokePermission(tx) {
+  for (var i = 0; i < tx.device.users.length; i++){
+    if(tx.removedUser.userId === tx.device.users[i].userId) {
+      tx.device.users.splice(i, 1); 
+    }
+  }
+
+  const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
+  await assetRegistry.update(tx.device);
 }

392b495a7e73668224eec1a4307f2dbec68482e7
Thiago Fontes <thiago93br@gmail.com>
2020-02-12 00:39:44 -0300

    Redefining access permissions

--- network/lib/logic.js
+++ network/lib/logic.js
@@ -1,15 +1,20 @@
 /**
- * Track the device acess permission and ownership 
- * @param {dev.thiagofontes.givePermission} tx - the device to be processed
+ * Sample transaction processor function.
+ * @param {dev.thiagofontes.givePermission} tx The sample transaction instance.
  * @transaction
  */
 async function givePermission(tx) {
   tx.device.users.push(tx.addedUser);
-
+  
   const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
   await assetRegistry.update(tx.device);
 }
 
+/**
+ * Sample transaction processor function.
+ * @param {dev.thiagofontes.revokePermission} tx The sample transaction instance.
+ * @transaction
+ */
 async function revokePermission(tx) {
   for (var i = 0; i < tx.device.users.length; i++){
     if(tx.removedUser.userId === tx.device.users[i].userId) {

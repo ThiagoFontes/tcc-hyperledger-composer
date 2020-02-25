2511321c72de950e48e5e17b00194aa421f9414f
Thiago Fontes <thiago93br@gmail.com>
2020-02-25 10:58:57 -0300

    Adding changeOwner function and improving cold start script

--- devices-network/cold_start_network.sh
+++ devices-network/cold_start_network.sh
@@ -1,10 +1,20 @@
+# Restart Fabric
 ../fabric-dev-servers/stopFabric.sh
 ../fabric-dev-servers/startFabric.sh
+
+# Create peer admin card
 ../fabric-dev-servers/createPeerAdminCard.sh
 
+# Instaling and starting devices-network
 composer network install --card PeerAdmin@hlfv1 --archiveFile devices-network@0.0.1.bna
 composer network start --networkName devices-network --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+
+# Deleting old card if exists and importing a new one
 composer card delete --card admin@devices-network
 composer card import --file networkadmin.card
+
+# Testing configuration
 composer network ping --card admin@devices-network
 
+# Start REST server
+composer-rest-server -c admin@devices-network -n never -u true -w true
--- devices-network/lib/logic.js
+++ devices-network/lib/logic.js
@@ -23,24 +23,37 @@
  * @transaction
  */
 async function givePermission(tx) {
-    tx.device.users.push(tx.addedUser);
-    
-    const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
-    await assetRegistry.update(tx.device);
-  }
+  tx.device.users.push(tx.addedUser);
   
-  /**
-   * Sample transaction processor function.
-   * @param {dev.thiagofontes.revokePermission} tx The sample transaction instance.
-   * @transaction
-   */
-  async function revokePermission(tx) {
-    for (var i = 0; i < tx.device.users.length; i++){
-      if(tx.removedUser.userId === tx.device.users[i].userId) {
-        tx.device.users.splice(i, 1); 
-      }
+  const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
+  await assetRegistry.update(tx.device);
+}
+  
+/**
+ * Sample transaction processor function.
+ * @param {dev.thiagofontes.revokePermission} tx The sample transaction instance.
+ * @transaction
+ */
+async function revokePermission(tx) {
+  for (var i = 0; i < tx.device.users.length; i++){
+    if(tx.removedUser.userId === tx.device.users[i].userId) {
+      tx.device.users.splice(i, 1); 
     }
-  
-    const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
-    await assetRegistry.update(tx.device);
   }
+
+  const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
+  await assetRegistry.update(tx.device);
+}
+
+
+/**
+ * Sample transaction processor function.
+ * @param {dev.thiagofontes.changeOwner} tx The sample transaction instance.
+ * @transaction
+ */
+async function changeOwner(tx) {
+  tx.device.owner = tx.newOwner;
+
+  const assetRegistry = await getAssetRegistry('dev.thiagofontes.Device');
+  await assetRegistry.update(tx.device);
+}

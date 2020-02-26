1cb5a067e1ec2843af7da050c5b95fa7ecae362d
Thiago Fontes <thiago93br@gmail.com>
2020-02-25 22:17:28 -0300

    Adding function to decrypt files and uptating creation scritps with extracted data

--- Hardware/Bluetooh door/src/main.cpp
+++ Hardware/Bluetooh door/src/main.cpp
@@ -8,6 +8,8 @@
 //http://www.lucadentella.it/en/2017/02/10/esp32-10-generatore-numeri-random/
 #define DR_REG_RNG_BASE 0x3ff75144
 
+const std::string key_string = "4To4jyTAOMvf99fx";
+
 // Function that generates a random string given a lenght
 std::string get_random_string( size_t length )
 {
@@ -27,11 +29,10 @@ std::string get_random_string( size_t length )
 }
 
 // Function that encrypt a string message
-std::string encrypt_message(std::string random_message) {
+std::string encrypt(std::string random_message) {
   mbedtls_aes_context aes;
 
-  //Defining our key and translating it into char array
-  std::string key_string = "4To4jyTAOMvf99fx";
+  //Translating our key into char array
   char key[key_string.size()+1];
   strcpy(key, key_string.c_str());
 
@@ -47,28 +48,64 @@ std::string encrypt_message(std::string random_message) {
   mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, TXT_VALID_LENGTH, iv,(const unsigned char*)input, output);
   mbedtls_aes_free( &aes );
 
-
-  Serial.print("Encrypted message: ");
-  for (int i = 0; i < strlen((const char*) output); i++) {
-    char str[3];
-
-    sprintf(str, "%02x", (int)output[i]);
-    Serial.print(str);
-  }
-
-  Serial.printf("\nkey: %s\n", key);
-  Serial.printf("Message: %s\n", input);
-
   std::string output_string((char*)output);
 
   return output_string;
 }
 
+std::string decrypt(std::string encrypeted_string){
+   //Translating our key into char array
+  char key[key_string.size()+1];
+  strcpy(key, key_string.c_str());
+
+  char input[encrypeted_string.size()+1];
+  strcpy(input, encrypeted_string.c_str());
+
+  unsigned char iv[IV_SIZE] = {0};
+  unsigned char output[INPUT_SIZE] = {0};
+
+  mbedtls_aes_context aes;
+
+  mbedtls_aes_init( &aes );
+  mbedtls_aes_setkey_dec( &aes, (const unsigned char*) key, strlen(key) * 8 );
+  int TXT_VALID_LENGTH = ((strlen((const char*)input)%16 == 0)  ?  strlen((const char*)input) :  (strlen((const char*)input)/16 + 1)*16);
+  mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, TXT_VALID_LENGTH, iv,(const unsigned char*)input, output);
+  mbedtls_aes_free( &aes );
+
+  std::string output_string((char*)output);
+  return output_string;
+}
+
+void print_string(std::string message, std::string string_message) {
+  char char_array1[message.size()+1];
+  strcpy(char_array1, message.c_str());
+  char char_array2[string_message.size()+1];
+  strcpy(char_array2, string_message.c_str());
+
+  Serial.printf("%s: %s\n", char_array1, char_array2);
+}
+
+void print_as_hex(std::string encrypeted_string) {
+  Serial.print("Encrypted String as HEX: ");
+  char char_array[encrypeted_string.size()+1];
+  strcpy(char_array, encrypeted_string.c_str());
+  for (int i = 0; i < strlen((const char*) char_array); i++) {
+    char str[3];
+    sprintf(str, "%02x", (int)char_array[i]);
+    Serial.print(str);
+  }
+  Serial.println();
+}
+
 void setup() {
   Serial.begin(115200);
-
+  print_string("Key", key_string);
   std::string random_string = get_random_string(32);
-  std::string encrypeted_string = encrypt_message(random_string);
+  print_string("Random String", random_string);
+  std::string encrypeted_string = encrypt(random_string);
+  print_as_hex(encrypeted_string);
+  std::string decrypted_string = decrypt(encrypeted_string);
+  print_string("Decrypted String", decrypted_string);
 }
 
 void loop() {
--- create_10000_devices.sh
+++ create_10000_devices.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 counter=0
 
-while [ $counter -lt 50 ]
+while [ $counter -lt 1000 ]
 do
-    counter=$((counter + 1))
     printf "iotDevice"$counter";"
     curl --location --request POST 'http://localhost:3000/api/Device' --header 'Content-Type: application/json' --data-raw '{
         "$class": "dev.thiagofontes.Device",
         "deviceId": "iotDevice'$counter'",
         "model": "12341",
         "encryptionKey": "'$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)'",
-        "owner": "resource:dev.thiagofontes.User#u1",
-        "users": ["resource:dev.thiagofontes.User#u1"]
+        "owner": "resource:dev.thiagofontes.User#user1",
+        "users": ["resource:dev.thiagofontes.User#user1"]
         }' -w "%{time_starttransfer}\n" -s -o /dev/null
+    counter=$((counter + 1))
 done
